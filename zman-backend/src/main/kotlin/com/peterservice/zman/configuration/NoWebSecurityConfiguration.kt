5c10ed8b783731c4e0225a3e94d3c5614f13fba7
alexey.borodin <alexey.borodin@billing.ru>
2019-02-11 14:43:33 +0300

    fix for encoded slashes

--- zman-backend/src/main/kotlin/com/peterservice/zman/configuration/NoWebSecurityConfiguration.kt
+++ zman-backend/src/main/kotlin/com/peterservice/zman/configuration/NoWebSecurityConfiguration.kt
@@ -1,9 +1,13 @@
 package com.peterservice.zman.configuration
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.firewall.DefaultHttpFirewall
+import org.springframework.security.web.firewall.HttpFirewall
 
 @ConditionalOnExpression("'\${authentication.type}' == 'NONE'")
 @Configuration
@@ -20,5 +24,17 @@ open class NoWebSecurityConfiguration : WebSecurityConfigurerAdapter() {
             .and()
             .csrf()
             .disable()
+    }
+
+    @Bean
+    open fun allowUrlEncodedSlashHttpFirewall(): HttpFirewall {
+        val firewall = DefaultHttpFirewall()
+        firewall.setAllowUrlEncodedSlash(true)
+        return firewall
+    }
+
+    @Throws(Exception::class)
+    override fun configure(web: WebSecurity) {
+        web.httpFirewall(allowUrlEncodedSlashHttpFirewall())
     }
 }
