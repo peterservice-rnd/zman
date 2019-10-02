a0878b7f95d5798b6579f2287a3b7d65cd9eb5ca
Andrei Hlubokau <andrei.hlubokau@it-wk.ru>
2019-10-02 17:40:47 +0300

    remove old controller

--- zman-backend/src/main/kotlin/com/peterservice/zman/api/services/AuthenticationFacade.kt
+++
@@ -1,7 +0,0 @@
-package com.peterservice.zman.api.services
-
-import org.springframework.security.core.Authentication
-
-interface AuthenticationFacade {
-    fun getAuthentication(): Authentication
-}
